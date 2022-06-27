ae9ad5d6fa1bdda42dcf631ae41ab1ff36f013c2
rezzco <rezakingdom@gmail.com>
2022-06-27 02:09:25 -0400

    added features to the game
    Random function, images, subimages to extract game objects.

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,23 +1,44 @@
 package main;
 
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Game extends JFrame{
 	
 	private GameScreen gameScreen;
-	
+	private BufferedImage img;
+
 	public Game() {
+		super();
+
+		importImage();
 		
-		setSize(400, 400);
+		setSize(640, 640);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		gameScreen = new GameScreen();
+		gameScreen = new GameScreen(img);
 		add(gameScreen);
 	}
 
+	private void importImage() {
+		InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
 	public static void main(String[] args) {
-		System.out.println("hello there");
 		Game g = new Game();
 		
 
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -1,24 +1,60 @@
 package main;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
-public class GameScreen extends JPanel{
+public class GameScreen extends JPanel {
 
-	public GameScreen() {
-		
+	private Random random;
+	private BufferedImage img;
+
+	private ArrayList<BufferedImage> sprites = new ArrayList<>();
+
+	public GameScreen(BufferedImage img) {
+		super(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		this.img = img;
+		loadSprites();
+		random = new Random();
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		g.setColor(Color.RED);
-		g.fillRect(60, 60, 80, 80);
-		g.drawRect(50, 50, 100, 100);
-	}
-	
 
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+
+				g.setColor(getRandomColore());
+				//g.fillRect(i * 32, j * 32, 32, 32);
+				g.drawImage(sprites.get(getRandomIndex()), i * 32, j * 32, null);
+			}
+		}
+//		BufferedImage ork = img.getSubimage(0, 32, 32, 32);
+		
+
+	}
+
+	private Color getRandomColore() {
+		return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+	}
+
+	private int getRandomIndex() {
+		return random.nextInt(sprites.size());
+	}
+	private void loadSprites() {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 10; j++) {
+				sprites.add(img.getSubimage(j * 32, i * 32, 32, 32));
+			}
+
+		}
+
+	}
 }
